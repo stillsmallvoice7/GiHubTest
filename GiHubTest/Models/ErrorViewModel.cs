1e3c854473fde638b6e2afbe689ff599c0906aa7
Chris Lasmanis <chrislasmanis@hotmail.com>
2021-06-09 19:18:09 -0700

    Add project files.

---
+++ GiHubTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GiHubTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
